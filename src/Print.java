90adfe6066a303d6faa56e68e89ee06ccd9b5bc3
Albina Taalaibek <albinatkyzy@gmail.com>
2023-12-09 23:18:55 +0600

    my 2nd push

--- src/Print.java
+++ src/Print.java
@@ -1,5 +1,9 @@
 public class Print {
     public static void main(String[] args) {
         System.out.println("Hello Snowman!");
+
+        for (int i=0; i<5; i++){
+            //mu first change
+        }
     }
 }
